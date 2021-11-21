282cccd559186c361d2e5f6180785d933eb4be16
FukushiKen2 <73925597+FukushiKen2@users.noreply.github.com>
2021-11-21 21:47:49 +0900

    Update main.py

--- main.py
+++ main.py
@@ -28,6 +28,23 @@ def hello_world():
 def masayuki():
     return "hatano"
 
+	
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    return 'OK'
+
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
