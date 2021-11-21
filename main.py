2978ef48691a2c6812121ed818c1cf0056dbe568
FukushiKen2 <73925597+FukushiKen2@users.noreply.github.com>
2021-11-21 20:58:47 +0900

    Update main.py

--- main.py
+++ main.py
@@ -13,54 +13,35 @@ import os
 
 app = Flask(__name__)
 
-#環境変数取得
-# LINE Developersで設定されているアクセストークンとChannel Secretをを取得し、設定します。
-YOUR_CHANNEL_ACCESS_TOKEN = os.environ["YOUR_CHANNEL_ACCESS_TOKEN"]
-YOUR_CHANNEL_SECRET = os.environ["YOUR_CHANNEL_SECRET"]
+YOUR_CHANNEL_ACCESS_TOKEN = "yffJy2EGKvFKla5Bgk+H4jez+6qeexJe54oE4uyn3GGk4rW6DoeUT6hr+EMeY8rvTRhxUG9HTQQ+Z9l6H+Y+atNsIvOQsp026q2TdJnPVAybdrhJHd2ngNc5Lg6OUuQfOS7IALNEecrnO4X9p02yXwdB04t89/1O/w1cDnyilFU="
+YOUR_CHANNEL_SECRET = "ee66fe29987b4811e91c4a88e4d5b0fd"
 
 line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(YOUR_CHANNEL_SECRET)
 
 
-## 1 ##
-#Webhookからのリクエストをチェックします。
+
 @app.route("/callback", methods=['POST'])
 def callback():
-    # リクエストヘッダーから署名検証のための値を取得します。
+
     signature = request.headers['X-Line-Signature']
 
-    # リクエストボディを取得します。
+   
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
 
-    # handle webhook body
-　# 署名を検証し、問題なければhandleに定義されている関数を呼び出す。
+
     try:
         handler.handle(body, signature)
-　# 署名検証で失敗した場合、例外を出す。
+
     except InvalidSignatureError:
         abort(400)
-　# handleの処理を終えればOK
     return 'OK'
 
-## 2 ##
-###############################################
-#LINEのメッセージの取得と返信内容の設定(オウム返し)
-###############################################
-
-#LINEでMessageEvent（普通のメッセージを送信された場合）が起こった場合に、
-#def以下の関数を実行します。
-#reply_messageの第一引数のevent.reply_tokenは、イベントの応答に用いるトークンです。 
-#第二引数には、linebot.modelsに定義されている返信用のTextSendMessageオブジェクトを渡しています。
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=event.message.text)) #ここでオウム返しのメッセージを返します。
+        TextSendMessage(text=event.message.text)) 
 
-# ポート番号の設定
-if __name__ == "__main__":
-#    app.run()
-    port = int(os.getenv("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
