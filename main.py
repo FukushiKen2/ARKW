3eee21eecd83815d3c8c5050c611ef9d726a0b2c
FukushiKen2 <73925597+FukushiKen2@users.noreply.github.com>
2021-11-21 18:28:10 +0900

    Update main.py

--- main.py
+++ main.py
@@ -13,13 +13,7 @@ def hello():
 def index():
 	return render_template("index.html")
 
-@app/route("/index",method=["GETS"])
-def receive_get():
-	name = request.args["my_name"]
-	if len(name) == 0:
-		return "名前が未入力"
-	else:
-		return "名前は" + str(name) + "です"
+
 
 
 if __name__ == "__main__":
