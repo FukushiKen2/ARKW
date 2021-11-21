7ecb94d446478561886d4fa56954955731802514
Fukushiken2 <fukushi19960929@gmail.com>
2021-11-21 18:23:56 +0900

    h

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from flask import Flask
 from flask import render_template
+from flask import request
+
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -9,6 +12,15 @@ def hello():
 @app.route("/index")
 def index():
 	return render_template("index.html")
+
+@app/route("/index",method=["GETS"])
+def receive_get():
+	name = request.args["my_name"]
+	if len(name) == 0:
+		return "名前が未入力"
+	else:
+		return "名前は" + str(name) + "です"
+
 
 if __name__ == "__main__":
     app.run(debug=True)                        
