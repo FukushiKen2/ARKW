3c954060cd833ba5675afa3128c5c15f647078cb
FukushiKen2 <73925597+FukushiKen2@users.noreply.github.com>
2021-11-21 17:32:48 +0900

    Update main.py

--- main.py
+++ main.py
@@ -3,14 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    name = "Hello World"
-    return name
+        return "Hello, World!"
 
-@app.route('/good')
-def good():
-    name = "Good"
-    return name
-
-## おまじない
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True)                        
