f241ede22a9c4b9755ce3dec8094bd808bbc5e32
FukushiKen2 <73925597+FukushiKen2@users.noreply.github.com>
2021-11-21 21:46:33 +0900

    Update main.py

--- main.py
+++ main.py
@@ -24,26 +24,10 @@ handler = WebhookHandler(YOUR_CHANNEL_SECRET)
 def hello_world():
     return "hello world!"
 
-@app.route("/callback", methods=['POST'])
+@app.route("/callback")
 def masayuki():
     return "hatano"
-	
-	
-def callback():
-    # get X-Line-Signature header value
-    signature = request.headers['X-Line-Signature']
 
-    # get request body as text
-    body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
-
-    # handle webhook body
-    try:
-        handler.handle(body, signature)
-    except InvalidSignatureError:
-        abort(400)
-
-    return 'OK'
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
