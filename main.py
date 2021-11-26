344c82a2dd8c524885eaba694481c5939268ac1c
FukushiKen2 <73925597+FukushiKen2@users.noreply.github.com>
2021-11-26 16:22:27 +0900

    Update main.py

--- main.py
+++ main.py
@@ -85,9 +85,9 @@ def handle_message(event):
 #    f.write("\n")
 #    f.close()
 
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=event.message.text))
+    #line_bot_api.reply_message(
+        #event.reply_token,
+        #TextSendMessage(text=event.message.text))
 
     
     conn = psycopg2.connect("postgres://nadqddfmbwcnoy:580a674ac5c8a0bd50cbca87847170ee782770068e861101bac898f351bb997e@ec2-35-169-204-98.compute-1.amazonaws.com:5432/d6379htn7887en", sslmode='require'
